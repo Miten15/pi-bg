131bc5f18d50f7a004acee0112c749eb29424db6
Miten <miten2006@gmail.com>
2024-03-11 09:03:38 +0530

    auth

--- server.js
+++ server.js
@@ -1,14 +1,26 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
 const dotenv = require('dotenv');
-const port = 5003;
 const mongoose = require("mongoose");
+const errorHandler = require('./middleware/errorHandling');
+const authRouter = require("./routes/auth");
+const userRouter = require("./routes/user");
+const port = 5003
+
 
 dotenv.config();
-
 mongoose.connect(process.env.MONGO_URL)
-  .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.error("MongoDB connection error:", err));
+.then(()=> console.log("database connected"))
+.catch((err)=> console.log(err))
 
-app.get('/', (req, res) => res.send('Hello World!'));
-app.listen(process.env.PORT || port, () => console.log(`Example app listening on port ${process.env.PORT || port}!`))
+
+app.use(express.json({limit: '10mb'}));
+app.use(express.urlencoded({limit: "10mb", extended: true}));
+
+app.use(errorHandler);
+app.use('/api/', authRouter);
+app.use('/api/users', userRouter);
+
+
+app.get('/', (req, res) => res.send('Hello World!'))
+app.listen(process.env.PORT || port, () => console.log(`Example app listening on port ${process.env.PORT }!`))
